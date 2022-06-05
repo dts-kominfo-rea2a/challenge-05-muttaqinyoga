7de2fb1a9ceaa71ef0c69b2e43f98718a0d6112c
muttaqinyoga <muttaqinyoga@gmail.com>
2022-06-05 23:09:54 +0700

    menyelesaikan tugas sesuai instruksi

--- index.js
+++ index.js
@@ -6,15 +6,23 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (listName, sortCallBack) => {
+  let callBackResult = sortCallBack(listName);
+
+  let results = [];
+  for(let i = 0; i<callBackResult.length; i++){
+    results.push(i+1+'. '+callBackResult[i]);
+  }
+  return results;
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = listName => listName.sort();
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = listName => listName.sort().reverse();
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
